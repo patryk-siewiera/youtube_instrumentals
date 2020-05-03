7a08a26e5375670d44f33ce65b2ad3c2e5ccf13f
patryk-siewiera <patryk.siewiera@gmail.com>
2020-05-03 10:32:22 +0200

    gui working

---
+++ clean_my_project.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+import shutil
+import time
+
+
+def create_folder(folder_name):
+    print('\n_* def create_folder():  INPUT == ', folder_name, '\n')
+    try:
+        os.mkdir(folder_name)
+        print(folder_name, ' -> ++ folder successful created')
+    except:
+        print(folder_name, ' -> -- folder already created')
+    return folder_name
+
+
+def move_files(extension, folder):
+    print('\n_* def move_files():  INPUT ==  ext==', extension, " folder==", folder)
+    sourcepath = Path().absolute()
+    sourcefiles = os.listdir(sourcepath)
+    destination = str(sourcepath) + "\\" + folder
+
+    print("list of all files: \n", sourcefiles)
+
+    print("source:", sourcepath)
+    print("destination:", destination)
+
+    for file in sourcefiles:
+        if file.endswith(extension):
+            shutil.move(os.path.join(sourcepath, file), os.path.join(destination, file))
+
+
+def remove_delete_download():
+    print('\n')
+    try:
+        shutil.rmtree('!delete')
+    except:
+        print('---')
+    try:
+        shutil.rmtree('!download')
+    except:
+        print('---')
+
+
+move_files('.wav', create_folder('!delete'))
+
+move_files('.part', create_folder('!delete'))
+
+remove_delete_download()
---
+++ init.py
@@ -0,0 +1,111 @@
+from __future__ import unicode_literals
+import youtube_dl
+import validators
+import PySimpleGUI as sg
+
+
+def gui():
+    sg.theme('dark')  # theme color
+
+    layout = [[sg.Text('Top line text')],
+              [sg.InputText(), sg.Text('url'), ],
+              [sg.InputText(), sg.Text('url2')],
+              [sg.InputText(), sg.Text('keyword')],
+              [sg.InputText(), sg.Text('keyword2')],
+              [sg.Submit(), sg.Cancel()]]
+
+    window = sg.Window('Window Title', layout, keep_on_top=True, icon="ico.ico")
+    # TODO: keep on top flag
+
+    # values -> text from text boxes -> dictionary
+    # event -> what button was clicked
+    event, values = window.read()
+    window.close()
+
+    if event == 'Cancel':
+        print('if -> exit')
+        exit()
+
+    print('\n_* def gui():  RETURN == ', values, '\n')
+    return values
+
+
+def gui_second():
+    layout2 = [[sg.Text('Second Window')],
+               [sg.Ok()]]
+    window2 = sg.Window('Answer', layout2)
+    event2, values2 = window2.read()
+    window2.close()
+
+
+def validate(url):
+    print('\n_* def validate():  INPUT == ', url, '\n')
+    output = []
+    url_list = []
+    key_word_list = []
+
+    for i in range(len(url)):
+        if url[i] == '':
+            continue
+        check = validators.url(url[i])
+        if not check:
+            print('  --- wrong URL (False) --- ', url[i])
+            output.append(False)
+            key_word_list.append(url[i])
+        if check:
+            print('  +++ correct URL (True) +++ ', url[i])
+            output.append(True)
+            url_list.append(url[i])
+
+    # return list of True / False
+    end = [url_list, key_word_list]
+    print('\n_* def validate():  RETURN == ', end, '\n')
+    return end
+
+
+def ydl(url_keyword):
+    url = url_keyword[0]
+    keyword = url_keyword[1]
+
+    ydl_opts_wav = {
+        'format': 'bestaudio/best',
+        'outtmpl': '!download/%(uploader)s/%(title)s.%(ext)s',
+        'min_views': 10000,
+        'max_views': 10000000000000,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '320',
+        }],
+    }
+
+    print('\n_* def ydl(): INPUT url== ', url)
+    for i in range(len(url)):
+        print('\ndownload: ', url[i])
+        try:
+            youtube_dl.YoutubeDL(ydl_opts_wav).download([url[i]])
+        except:
+            print('EXCEPT: youtube-dl unsupported URL')
+
+    print('\n_* def ydl(): INPUT keywords== ', keyword)
+    for i in range(len(keyword)):
+        print('\ndownload: ', keyword[i])
+        try:
+            youtube_dl.YoutubeDL(ydl_opts_wav).extract_info("ytsearch:" + keyword[i])
+        except:
+            print('EXCEPT: youtube-dl unsupported keyword')
+
+    # TODO: progress hooks
+
+    # TODO: download links
+
+
+mock_psg = {0: 'sdf][4]3212\[]\[',
+            1: 'https://youtu.be/DfZPeUujmIk',
+            2: 'https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_All_Widgets.py',
+            3: 'tame impala',
+            4: 'skrillex'}
+
+input_values = gui()
+links = validate(input_values)
+ydl(links)
