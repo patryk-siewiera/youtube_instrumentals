ac0c5d3fd3fb06430f9eb6516f7e3c0c9d35d87e
psiewiera <patryk.siewiera@gmail.com>
2020-12-04 16:28:50 +0100

    code refactor -> migrate everything as classes (start form clean_my_project.py)

--- youtube_instrumentals/clean_my_project.py
+++ youtube_instrumentals/clean_my_project.py
@@ -1,46 +1,55 @@
 import os
-from pathlib import Path
 import shutil
-import time
+from pathlib import Path
 
 
-def create_folder(folder_name):
-    """create folders specified in argument"""
-    print('\n_* def create_folder():  INPUT == ', folder_name, '\n')
-
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-
-    return folder_name
-
-
-def move_files(extension, folder):
-    """move files with extension to specific folder"""
-    print('\n_* def move_files():  INPUT ==  ext==', extension, " folder==", folder)
-    sourcepath = Path().absolute()
-    sourcefiles = os.listdir(sourcepath)
-    destination = str(sourcepath.joinpath(folder))
-
-    for file in sourcefiles:
-        if file.endswith(extension):
-            shutil.move(os.path.join(sourcepath, file), os.path.join(destination, file))
-
-
-def remove_delete_download(folder):
-    """delete folders with all content from:"""
-    print('\n')
-    try:
-        shutil.rmtree(folder)
-    except FileNotFoundError:
-        print('Cannot delete: ', folder)
-
-
-def main():
+class OrganizeDownloadedFiles:
     """
-    removes all downloaded files
+    this is doctype for class -> ctrl + Q
     """
-    move_files('.part', create_folder('!delete'))
-    move_files('.ytdl', create_folder('!delete'))
-    move_files('.wav', create_folder('!delete'))
-    remove_delete_download("!delete")
-    remove_delete_download("!audio")
+
+    def __init__(self, create_new_folder, extensions_moved_to_new_folder, remove_folder_and_content_inside):
+        self.create_fld = create_new_folder
+        self.extensions = extensions_moved_to_new_folder
+        self.remove_fld = remove_folder_and_content_inside
+
+    def create_folder(self):
+        """create folders specified in argument"""
+        print('\n_* def create_folder():  INPUT == ', self.create_fld, '\n')
+        if not os.path.exists(self.create_fld):
+            os.makedirs(self.create_fld)
+
+        return self.create_fld
+
+    def move_files(self):
+        """move files with extension to specific folder"""
+        print('\n_* def move_files():  INPUT ==  ext==', self.extensions, " folder==", self.create_fld)
+        sourcepath = Path().absolute()
+        sourcefiles = os.listdir(sourcepath)
+        destination = str(sourcepath.joinpath(self.create_fld))
+
+        for file in sourcefiles:
+            for ext_iterable in self.extensions:
+                if file.endswith(ext_iterable):
+                    shutil.move(os.path.join(sourcepath, file), os.path.join(destination, file))
+
+    def remove_delete_download(self):
+        """delete folders with all content from:"""
+        print('\n')
+        for fld_iterable in self.remove_fld:
+            try:
+                shutil.rmtree(fld_iterable)
+            except FileNotFoundError:
+                print('Cannot delete: ', fld_iterable)
+
+
+# TODO: create logs via debbuging python?
+
+# TODO: resume_here
+
+# removes all downloaded files + partly and undone
+organize = OrganizeDownloadedFiles('!delete', ['.part', '.ytdl', '.wav'], ['!delete', 'audio'])
+organize.create_folder()
+organize.move_files()
+organize.remove_delete_download()
+# OrganizeDownloadedFiles.remove_delete_download('audio')
